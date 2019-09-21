b2d9c553e46c3a445b73dd1c8ed52a2cc9d78aa6
Rory Murray <rory.christian.murray@gmail.com>
2019-09-21 08:27:34 -0400

    Add custom error handling middleware exampple

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ server.use(express.json()); // parse incoming json
 server.use(cors()); // allow cors
 
 server.use("/api/posts", postRoutes);
-server.use("/api", (req, res) => res.send("API running"));
+// server.use("/api", (req, res) => res.send("API running"));
+
+// Write a request handler that responds with a custom message
+// for invalid URLs
+
+server.use(function(req, res) {
+  res.status(404).send("Aint nobody got time for that!");
+});
 
 const port = 8000;
 server.listen(port, () => {
